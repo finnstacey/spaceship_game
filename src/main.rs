f1e2e3691230093c3ab37fb9329b5a9461ac889b
Finn Stacey <finn.stacey@subnivean.au>
2023-12-27 17:36:11 +1100

    refactoring of code and intro to using bundles (built-in ones and custom defined)

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,21 @@
+mod debug;
+mod movement;
+mod spaceship;
+
 use bevy::prelude::*;
-
-#[derive(Component, Debug)]
-struct Position {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Component, Debug)]
-struct Velocity {
-    x: f32,
-    y: f32,
-}
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
 
 fn main() {
     App::new()
-    .add_systems(Startup, spawn_spaceship)
-    .add_systems(Update, (update_position, print_position))
     .add_plugins(DefaultPlugins)
+    .add_plugins(SpaceshipPlugin)
+    .add_plugins(MovementPlugin)
+    .add_plugins(DebugPlugin)
     .run();
 }
 
-fn spawn_spaceship(mut commands: Commands) {
-    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 1.0}));
-}
 
-fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
-    for (velocity, mut position) in query.iter_mut() {
-        position.x += velocity.x;
-        position.y += velocity.y;
-    }
-}
 
-fn print_position(query: Query<(Entity, &Position)>) {
-    for (entity, position) in query.iter() {
-        info!("Entity {:?} is at position {:?}", entity, position)
-    }
-}
+
